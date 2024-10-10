8614f871c36238c5d331aef0a1546e159cdf633d
Sascha Kaufmann <skaufmann@ventronic.com>
2024-10-10 13:38:10 +0200

    fix: i dont know why it doesnt work

--- update.sh
+++ update.sh
@@ -1,7 +1,8 @@
 cd ..
 cd ..
-#git submodule foreach git pull 
 git pull --recurse-submodules
+git submodule update --init
+git submodule foreach git pull origin main
 npm install
 git add .
 git commit -m "ci: update submodules"
