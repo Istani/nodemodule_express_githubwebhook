f775471e2bbcdb4c9da1e57bbe3286f7489cbd50
Sascha Kaufmann <skaufmann@ventronic.com>
2024-10-10 13:48:15 +0200

    chore: next try

--- index.js
+++ index.js
@@ -5,12 +5,11 @@ const express = require('express');
 const router = express.Router();
 
 // Vielleicht kommen hier spater noch mehr urls, aber erstmal reicht eine!
-
 router.use(async (req, res, next) => {
-  require('child_process').execSync(`sh ${__dirname}/update.sh`);
+  var cmd = "sh " + __dirname + "}/update.sh";
+  debug.log("CMD: " + cmd);
+  require('child_process').execSync(cmd);
   res.send("OK");
 });
-
-
 
 module.exports = router;
--- update.sh
+++ update.sh
@@ -1,5 +1,6 @@
 cd ..
 cd ..
+pwd
 git pull --recurse-submodules
 git submodule update --init
 git submodule foreach git pull origin main
