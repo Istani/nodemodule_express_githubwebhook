9ec49b5249c2d7857d94a6c5e4138506cd8c291e
Sascha Kaufmann <skaufmann@ventronic.com>
2024-10-10 13:25:51 +0200

    fix: updateing script

--- index.js
+++ index.js
@@ -4,20 +4,11 @@ debug.log('Imported');
 const express = require('express');
 const router = express.Router();
 
-const {exec} = require('child_process');
-
 // Vielleicht kommen hier spater noch mehr urls, aber erstmal reicht eine!
 
 router.use(async (req, res, next) => {
-  exec(`sh ${__dirname}/update.sh`, (error, out) => {
-    if (error) {
-      console.log(`error: ${error.message}`);
-    } else if (out) {
-      console.log(`stderr: ${out}`);
-    }
-  });
-  
-  res.send("");
+  require('child_process').execSync(`sh ${__dirname}/update.sh`);
+  res.send("OK");
 });
 
 
--- update.sh
+++ update.sh
@@ -1,2 +1,7 @@
 cd ..
-git submodule foreach git pull 
+cd ..
+#git submodule foreach git pull 
+git pull --recurse-submodules
+npm install
+git add .
+git commit -m "ci: update submodules"
