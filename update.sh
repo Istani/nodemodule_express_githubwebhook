50228aac3f92185c0f1f6e9a1cbc984f80aa9b26
Sascha Kaufmann <skaufmann@ventronic.com>
2024-10-16 11:47:12 +0200

    fix: something went wrong and my changes are gone

--- update.sh
+++ update.sh
@@ -3,7 +3,9 @@
 pwd
 git pull --recurse-submodules
 git submodule update --init
+git submodule foreach git checkout main
 git submodule foreach git pull origin main
+git submodule foreach git checkout main
 npm install
 git add .
 git commit -m "ci: update submodules"
